031f2841eedb240605917ea528ed7aa75d116655
mehran razavi <mehranrazavi061@gmail.com>
2023-03-13 16:31:39 +0330

    fix bugs and merge

---
+++ src/api/middlewares/index.js
@@ -0,0 +1,9 @@
+const attachCurrentAdmin = require("./attachCurrentAdmin");
+const isAuth = require("./isAuth");
+const isCan = require("./isCan");
+
+module.exports = {
+  attachCurrentAdmin,
+  isAuth,
+  isCan
+}
