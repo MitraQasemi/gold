41b6cca46d4f8aa3d103dd231ee90d6c746a52f6
Mitra <mitraqasemi1999@gmail.com>
2023-03-27 16:57:12 +0430

    category crud verification

--- src/validation/categoryCrud.js
+++ src/validation/categoryCrud.js
@@ -2,15 +2,31 @@ const joi = require('joi');
 
 
 const create = {
-    body
+    body: joi.object().keys({
+        name: joi.string().required(),
+        category: joi.string().required(),
+        parent: joi.string().required(),
+        slug: joi.string().required()
+    })
+}
+
+const readMany = {
+    query: joi.object().keys({
+        size: joi.string().pattern(new RegExp('^[0-9]$')),
+        page: joi.string().pattern(new RegExp('^[0-9]$'))
+    })
 }
 
 const read = {
-
+    params: joi.object().keys({
+        id: joi.string().hex().length(24)
+    })
 }
 
 const Delete = {
-
+    params: joi.object().keys({
+        id: joi.string().hex().length(24)
+    })
 }
 
-module.exports = {create, read, Delete}
+module.exports = {create, readMany, read, Delete}
