142a5c7512abb3f952c949fa9702670a67c3432f
mehran razavi <mehranrazavi061@gmail.com>
2023-04-06 23:34:03 +0330

    computing route is added.but it's need some changes

--- src/services/user/gold.js
+++ src/services/user/gold.js
@@ -1,17 +1,31 @@
-const axios = require('axios');
+const axios = require("axios");
 const { ApiError } = require("../../api/middlewares/error");
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 require("dotenv").config({ path: "../.env" });
 
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient();
 
 const computing = async (type, weight, price) => {
-  try {   
-        
+  try {
+    const currentPrice = await prisma.goldPrice.findFirst();
+    if (type === "se-we" && weight) {
+      const totalPrice = currentPrice.sellQuotation * weight;
+      return totalPrice.toFixed(3);
+    } else if (type === "bu-we" && weight) {
+      const totalPrice = currentPrice.buyQuotation * weight;
+      return totalPrice.toFixed(3);
+    } else if (type === "se-pr" && price) {
+      const totalWeight = price / currentPrice.sellQuotation;
+      return totalWeight.toFixed(3);
+    } else if (type === "bu-pr" && price) {
+      const totalWeight = price / currentPrice.buyQuotation;
+      return totalWeight.toFixed(3);
+    } else {
+      return "there is no valid parameters";
+    }
   } catch (error) {
-        throw new ApiError(500, error.message);
+    throw new ApiError(400, "bad request");
   }
-}
+};
 
-module.exports = { computing};
+module.exports = { computing };
