fb26b1eac19bd17cec42a235263a0fc47c34371a
mehran razavi <mehranrazavi061@gmail.com>
2023-04-12 06:28:58 +0330

    gold transaction is completed

--- src/services/user/gold.js
+++ src/services/user/gold.js
@@ -8,9 +8,26 @@ moment.locale("fa");
 
 const prisma = new PrismaClient();
 
-const computing = async (type, weight, price) => {
-  try {
-    const currentPrice = await prisma.goldPrice.findFirst();
+const computing = async (type, value) => {
+  const currentPrice = await prisma.goldPrice.findFirst();
+  if (type === "sell-weight") {
+    const totalPrice = currentPrice.sellQuotation * value;
+    return totalPrice.toFixed(3);
+  } else if (type === "sell-price") {
+    const totalWeight = value / currentPrice.sellQuotation;
+    return totalWeight.toFixed(3);
+  } else if (type === "buy-weight") {
+    const totalPrice = currentPrice.buyQuotation * value;
+    return totalPrice.toFixed(3);
+  } else if (type === "buy-price") {
+    const totalWeight = value / currentPrice.buyQuotation;
+    return totalWeight.toFixed(3);
+  } else {
+    throw new ApiError(400, "bad request");
+  }
+};
+/**
+     const currentPrice = await prisma.goldPrice.findFirst();
     if (type === "se-we" && weight) {
       const totalPrice = currentPrice.sellQuotation * weight;
       return totalPrice.toFixed(3);
@@ -25,17 +42,12 @@ const computing = async (type, weight, price) => {
       return totalWeight.toFixed(3);
     } else {
       return "there is no valid parameters";
-    }
-  } catch (error) {
-    throw new ApiError(400, "bad request");
-  }
-};
-
-const buyGold = async (userId) => {
+ */
+const buyGold = async (userId, body) => {
   const now = moment();
   const currentHouer = now.format("HH:mm");
 
-  const config = await prisma.config.findFirstOrThrow({});
+  const config = await prisma.config.findFirstOrThrow();
 
   const currentLimitation = config.goldPurchaseLimit.find(
     (Lim) => Lim.startAt <= currentHouer && Lim.endAt > currentHouer
@@ -58,7 +70,61 @@ const buyGold = async (userId) => {
   });
 
   if (currentLimitation.weightLimit >= totalPurchasedGold._sum.expense) {
-    // ETC
+    return prisma.$transaction(async (prisma) => {
+      
+      const price = body.type === "buy-price" ? body.value : await computing(body.type, body.value);
+
+      const weight = await computing("buy-price", price);
+
+      const user = await prisma.user.findUniqueOrThrow({
+        where: {
+          id: userId,
+        },
+      });
+
+      if (user.walletBalance < price) {
+        throw new ApiError(403, "your wallet balance is not enough");
+      }
+
+      await prisma.user.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          goldBalance: {
+            increment: weight,
+          },
+          walletBalance: {
+            decrement: price,
+          },
+        },
+      });
+
+      await prisma.goldTransaction.create({
+        data: {
+          userId: user.id,
+          date: moment().toISOString(),
+          expense: weight,
+          status: "place holder",
+          paymentGateway: "place holder",
+          details: "place holder",
+        },
+      });
+
+      await prisma.walletTransaction.create({
+        data: {
+          userId: user.id,
+          date: moment().toISOString(),
+          expense: price,
+          status: "place holder",
+          paymentGateway: "place holder",
+          title: "place holder",
+          weight: weight,
+          quotation: 0.00, // 👈 place holder
+          details: "place holder",
+        },
+      });
+    });
   } else {
     throw new ApiError(403, "you can't buy gold anymore");
   }
