47afaa6a0e240570b9f73f35182b6c72c55e9140
mehran razavi <mehranrazavi061@gmail.com>
2023-05-01 15:28:26 +0330

    database designed

--- src/services/user/product.js
+++ src/services/user/product.js
@@ -3,19 +3,39 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-const buyGold = async (userId, productId) => {
+const buyProduct = async (userId, productId) => {
+  const product = await prisma.product.findUniqueOrThrow({
+    where: {
+      id: productId,
+    },
+  });
+  if (product.quantity === 0) {
+    throw new ApiError(400, "this product was sold out");
+  }
   const user = await prisma.user.findUniqueOrThrow({
     where: {
       id: userId,
     },
   });
 
-  const product = await prisma.product.findUniqueOrThrow({
-    where: {
-      id: productId,
-    },
-  });
+  const productPrice = await priceCalculator(product);
 
+  if (user.walletBalance < productPrice) {
+    throw new ApiError(400, "not enugh cash in wallet");
+  }
+  const transactionResult = await prisma.$transaction(async (prisma) => {
+    await prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        walletBalance: {
+          decrement: productPrice,
+        },
+      },
+    });
+    
+  });
 };
 
 const priceCalculator = async (product) => {
@@ -30,6 +50,6 @@ const priceCalculator = async (product) => {
 };
 
 module.exports = {
-  buyGold,
+  buyProduct,
   priceCalculator,
 };
