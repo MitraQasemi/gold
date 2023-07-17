3a99f0c8b168d2dcacf45675e71a21ca685b709e
mit <mitraqasemi1999@gmail.com>
2023-07-17 17:18:26 +0330

    chnage path in uploader

--- src/api/routes/v1/admin/product.js
+++ src/api/routes/v1/admin/product.js
@@ -62,7 +62,7 @@ const func = (app) => {
         try {
 
             const imagePath = `/public/products/${req.body.path}`;
-            const fullPath = path.join(process.cwd(), imagePath);
+            const fullPath = path.join("", imagePath);
             return res.sendFile(fullPath);
 
         } catch (error) {
