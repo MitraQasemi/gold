638994f409a1e9e5f42f9e7dcb9ddc650f9d62e7
mit <mitraqasemi1999@gmail.com>
2023-07-17 17:40:56 +0330

    chnage path in uploader

--- src/services/common/uploader.js
+++ src/services/common/uploader.js
@@ -10,7 +10,7 @@ module.exports = (req, directory) => {
       const form = formidable({ multiples: false });
       form.parse(req, (err, fields, file) => {
         if (err || !file.image) reject(err || new Error("Image is not provided."));
-        const newPath = path.join(`${_dirname}/../public`, directory, uuid.v4() + '_' + file.image.originalFilename)
+        const newPath = path.join(`${__dirname}/../public`, directory, uuid.v4() + '_' + file.image.originalFilename)
         const newPath2 = path.join(directory, uuid.v4() + '_' + file.image.originalFilename)
         console.log(newPath);
         fs.createReadStream(file.image.filepath)
