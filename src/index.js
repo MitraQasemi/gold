aa705fc71f9b1e5a04a36e5fb18b88ca0d10f1d7
mehran razavi <mehranrazavi061@gmail.com>
2023-07-29 15:50:15 +0330

    database seeder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const loader = require("./loaders/index");
-
+const { seeder } = require("./loaders/seeder");
 const express = require("express");
 
 const app = express();
 
 async function startServer() {
-    await loader(app);
+  await loader(app);
+  await seeder();
 }
 
 startServer();
---
+++ src/loaders/seeder.js
@@ -0,0 +1,88 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const productNames = ["ring", "earring", "Necklace", "wristband"];
+const installment = [
+  {
+    available: true,
+    deadLine: 120,
+    minWeight: 0.5,
+  },
+  {
+    available: false,
+    deadLine: 0,
+    minWeight: 0,
+  },
+];
+
+const seeder = async () => {
+  const productCount =await prisma.product.count({});
+  console.log(productCount);
+  if (productCount >= 100) {
+    return;
+  }
+  for (let i = 0; i < 100; i++) {
+    await prisma.product.create({
+      data: {
+        title: productNames[getRandomInt(productNames.length)],
+        description:
+          "Lorem ipsum dolor sit amet. Aut officiis autem 33 reprehenderit porro non obcaecati ullam. Sed sint magnam rem modi rerum qui commodi ullam est illo doloremque in rerum numquam a adipisci fugiat. Eum rerum nobis est laudantium ducimus in harum omnis. ",
+        category: productNames[getRandomInt(productNames.length)],
+        discount: getRandomInt(4) / 100,
+        profitPercentage: getRandomInt(7) / 100,
+        quantity: getRandomInt(51),
+        lockQuantity: 0,
+        tags: { set: ["gold", "ring", "wristband"] },
+        wage: getRandomInt(8) / 100,
+        installment: {
+          set: installment[getRandomInt(installment.length)],
+        },
+        variants: {
+          set: [
+            {
+              discount: 0.01,
+              installment: {
+                available: true,
+                deadLine: 120,
+                minWeight: 0.5,
+              },
+              lockQuantity: getRandomInt(11),
+              quantity: getRandomInt(100),
+              variantId: 1,
+              wage: getRandomInt(5) / 100,
+              weight: 2 + getRandomInt(5) / 10,
+              weightUnit: "buyQuotation",
+              variants: [],
+            },
+            {
+              discount: 0.01,
+              installment: {
+                available: false,
+                deadLine: 0,
+                minWeight: 0,
+              },
+              lockQuantity: getRandomInt(11),
+              quantity: getRandomInt(100),
+              variantId: 2,
+              wage: getRandomInt(5) / 100,
+              weight: 2 + getRandomInt(5) / 10,
+              weightUnit: "sellQuotation",
+              variants: [],
+            },
+          ],
+        },
+        weight: 2 + getRandomInt(5) / 10,
+        weightUnit: "buyQuotation",
+      },
+    });
+  }
+};
+
+module.exports = {
+  seeder,
+};
