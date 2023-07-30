2d907a7be4aeaf4e8623c01f2707e5177d1afaaf
mehran razavi <mehranrazavi061@gmail.com>
2023-07-30 14:06:43 +0330

    change adminAuth validation

--- src/validation/adminAuth.js
+++ src/validation/adminAuth.js
@@ -4,14 +4,14 @@ const joi = require('joi');
 const login = {
     body : joi.object().keys({
         username: joi.string().pattern(/^(?=.{5,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/).required(),
-        password: joi.string().pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/).required()
+        password: joi.string().pattern(/^[a-zA-Z0-9_.-]{8,}$/).required()
     })
 }
 
 const signup = {
     body : joi.object().keys({
         username: joi.string().pattern(/^(?=.{5,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/).required(),
-        password: joi.string().pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/).required(),
+        password: joi.string().pattern(/^[a-zA-Z0-9_.-]{8,}$/).required(),
         permissions: joi.array().items(joi.object({
             action: joi.string().valid(...["create", "read", "update", "delete"]).required(),
             subject: joi.string().valid(...["Admin", "User", "Product", "Category","Config","goldPrice"]).required()
