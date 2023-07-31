cb81c67b8b74ae43a05d2c90a44d2d180e97e18c
mit <mitraqasemi1999@gmail.com>
2023-07-31 19:30:57 +0330

    path fix

--- src/services/common/uploader.js
+++ src/services/common/uploader.js
@@ -11,10 +11,11 @@ module.exports = (req, directory) => {
       form.parse(req, (err, fields, file) => {
         if (err || !file.image) reject(err || new Error("Image is not provided."));
         const newPath = path.join('./src/public', directory, uuid.v4() + '_' + file.image.originalFilename)
-        const result = newPath.split("\\");
+        const result = newPath.split("//");
+        console.log(result);
           fs.createReadStream(file.image.filepath)
             .pipe(fs.createWriteStream(newPath))
-            .on("finish", () => resolve(`http://http://91.107.160.88:3001/${result[result.length - 1]}`))
+            .on("finish", () => resolve(`http://91.107.160.88:3001/${result[result.length - 1]}`))
             .on("error", (error) => reject(error));
       });
     });
