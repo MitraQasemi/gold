ba277b27ec56b61ba96276e88d737dfd6d7657ad
MitraQasemi <109904083+MitraQasemi@users.noreply.github.com>
2023-09-18 17:07:49 +0330

    Update goldPriceJob.js

--- src/jobs/goldPriceJob.js
+++ src/jobs/goldPriceJob.js
@@ -2,8 +2,8 @@
 // const myQueue = new Queue('my-queue-name');
 const CronJob = require('cron').CronJob;
 const { goldPrice } = require("./../services/admin/goldPrice")
-//every 2 hures 0 */2 * * *
-const job = new CronJob("*/20 * * * * *", async () => {
+//every 2 hours 0 */2 * * *
+const job = new CronJob("0 */2 * * *", async () => {
     await goldPrice();
     // myQueue.add({ message: '\n\nagold price updated!' });
 });
